475ce4aec8913ae72f0abaa910f0e9563eaba638
march4452 <78617037+march4452@users.noreply.github.com>
2021-03-09 20:17:46 +0700

    Add files via upload

---
+++ Program007.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace sadjang
+{
+    using System;
+
+    namespace sadjang
+    {
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("input mode :");
+                string m = Console.ReadLine();
+                Console.Write("input time :");
+                Double t = double.Parse(Console.ReadLine());
+                Console.Write("input price :");
+                double p = double.Parse(Console.ReadLine());
+                if ((m == "time") && (t > 0))
+                {
+                    Console.Write("{0}, {1}", t, p = Math.Pow(t - 1, 2));
+                }
+                else if (t < 0)
+                {
+                    Console.Write("Invalid mouse position");
+                }
+                else if (m == "price")
+                {
+                    Console.Write("{0},{1}", t = Math.Sqrt(p) + 1, p);
+                }
+                else if (m != "time" || m != "price")
+                {
+                    Console.Write("Invalid mode");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
