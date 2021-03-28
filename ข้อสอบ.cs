ef1b2f7b3e2081292f6eca5a114bd6b80aed6fc0
march4452 <78617037+march4452@users.noreply.github.com>
2021-03-28 22:27:34 +0700

    Add files via upload

---
+++ ข้อสอบ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x;
+            int y;
+            
+            x= a;
+            y= b;
+
+            if (x == y)
+            {
+                Console.WriteLine("x");
+            }
+            else
+            {
+                if (x < y)
+                { x = x + a; }
+                else
+                { y = y + b; }
+            }
+
+        }
+    }
+}
+
