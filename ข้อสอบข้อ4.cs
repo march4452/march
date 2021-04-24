b7062a2ac773167b2b6911b4fe1600ce916c5ef5
march4452 <78617037+march4452@users.noreply.github.com>
2021-04-24 15:13:26 +0700

    Add files via upload

---
+++ ข้อสอบข้อ4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x; float y; float dx; float dy; float step; float x1; float y1; float x2; float y2;
+            Console.Write("x1 :");
+            x1 = float.Parse(Console.ReadLine());
+            Console.Write("y1 :");
+            y1 = float.Parse(Console.ReadLine());
+            Console.Write("x2 :");
+            x2 = float.Parse(Console.ReadLine());
+            Console.Write("y2 :");
+            y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+            Math.Abs(dx); Math.Abs(dy);
+
+            if (dx >= dy)
+            {
+                step = dx;
+            }
+            else
+            {
+                step = dy;
+            }
+
+            dx /= step; dy /= step;
+            x = x1; y = y1;
+            int i = 1;
+            while (i <= step)
+            {
+
+            
+            
+            }
+
+
+            
+        }
+    }
+}
+
