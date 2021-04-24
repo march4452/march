7e0249d8974293f80435392602f56d115f8ce516
march4452 <78617037+march4452@users.noreply.github.com>
2021-04-24 16:01:29 +0700

    Add files via upload

---
+++ ข้อสอบข้อ6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+
+            int T = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= T; ++i)
+            {
+                for (int j = 1; j <= T; ++j)
+                {
+                    if (j <= T - i)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(A);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+        
+
+
+
+
+
+
+
+
+
+
+    }
+        
+}
+
