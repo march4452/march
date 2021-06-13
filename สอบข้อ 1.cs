e66efc9e4823c9413cacaac5f9052a33e91f089e
march4452 <78617037+march4452@users.noreply.github.com>
2021-06-13 15:09:01 +0700

    Add files via upload

---
+++ สอบข้อ 1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, e, f, g, h, i, j;
+            Console.WriteLine();
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            d = double.Parse(Console.ReadLine());
+            e = double.Parse(Console.ReadLine());
+            f = double.Parse(Console.ReadLine());
+            g = double.Parse(Console.ReadLine());
+            h = double.Parse(Console.ReadLine());
+            i = double.Parse(Console.ReadLine());
+            j = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}", a, b, c, d, e, f, g, h, i, j);
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            d = double.Parse(Console.ReadLine());
+            e = double.Parse(Console.ReadLine());
+            f = double.Parse(Console.ReadLine());
+            g = double.Parse(Console.ReadLine());
+            h = double.Parse(Console.ReadLine());
+            i = double.Parse(Console.ReadLine());
+            
+
+           
+            if (a == 0)
+            {
+                Console.WriteLine("The number Remaining");
+                Console.Clear();
+            }
+
+
+          
+            if (b == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+           
+            
+            if (c == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+
+
+            if (d == 0)
+            {
+                Console.WriteLine("The number is available");
+
+            }
+
+
+
+            
+            
+            if (e == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+
+            }
+
+
+
+            if (f == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+
+            
+            if (g == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+
+         
+            if (h == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+
+            if (i == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+
+
+
+           
+            if (j == 0)
+            {
+                Console.WriteLine("The number Remaining");
+
+            }
+            else
+                Console.WriteLine("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}", a, b, c, d, e, f, g, h, i);
+
+
+        }
+    }
+}
+       
+    
+
