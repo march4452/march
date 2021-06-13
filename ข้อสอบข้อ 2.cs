b4cc883f070cad4352019031bd52342a547204dd
march4452 <78617037+march4452@users.noreply.github.com>
2021-06-13 17:52:38 +0700

    Add files via upload

---
+++ ข้อสอบข้อ 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 63120501007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n >= 0)
+            {
+                Console.WriteLine("kuy");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("here");
+        }
+    }
+}
